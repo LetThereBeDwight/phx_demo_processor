965f1cb55b7067a0f47f67306b5de52280246181
Dwight Bell <dbell@surgeforward.com>
2020-03-30 15:32:20 -0700

    Manage multiple queues when monitoring queue processing in test

--- test/message_handler_test.exs
+++ test/message_handler_test.exs
@@ -2,38 +2,66 @@ defmodule PhxDemoProcessor.MessageHandlerTest do
   use ExUnit.Case
   alias PhxDemoProcessor.MessageHandler
 
-  defp monitor_processing(count, queue_name, last_processing_time \\ nil) do
+  defp monitor_processing(counts, last_processing_times \\ Map.new()) do
     handler_pid = Process.whereis(MessageHandler)
     :erlang.trace(handler_pid, true, [:receive])
 
-    process_time =
+    {counts, process_times} =
       receive do
-        {:trace, ^handler_pid, :receive, {:process_queue, ^queue_name}} ->
+        {:trace, ^handler_pid, :receive, {:process_queue, queue_name}} when :erlang.is_map_key(queue_name, counts)->
           time = System.monotonic_time(:millisecond)
-          if last_processing_time, do: assert time - last_processing_time >= 1000
-          time
+          if Map.has_key?(last_processing_times, queue_name) do
+            assert time - Map.get(last_processing_times, queue_name) >= 1000
+          end
+
+          { Map.put(counts, queue_name, Kernel.max(Map.get(counts, queue_name) - 1, 0)),
+            Map.put(last_processing_times, queue_name, time) }
       end
 
-    if count > 1 do
-      monitor_processing(count - 1, queue_name, process_time)
+    still_counting = Enum.reduce(counts, 0, fn {_k, v}, acc -> v + acc end)
+    if still_counting > 0 do
+      monitor_processing(counts, process_times)
     end
   end
 
   describe "Test Message Handling Genserver" do
     test "Single Message" do
-      assert :ok == MessageHandler.receive_message("TestHandlerQueue", "TestMessageHandler")
+      assert :ok == MessageHandler.receive_message("TestHandlerQueueSingle", "TestSingleMessage")
     end
 
     test "Simple Single Queue Rate Limiting" do
-      monitor_ref = spawn(fn -> monitor_processing(5, "TestHandlerQueue") end) |>
+      range = 1..5
+      monitor_ref = spawn(fn -> monitor_processing(%{"TestHandlerQueueRate" => Enum.count(range)}) end) |>
                     Process.monitor()
 
-      range = 0..4
       for i <- range do
-        assert :ok == MessageHandler.receive_message("TestHandlerQueue", "TestMessage_#{i}")
+        assert :ok == MessageHandler.receive_message("TestHandlerQueueRate", "TestMessage_#{i}")
       end
 
       assert_receive({:DOWN, ^monitor_ref, _, _, _}, 6000)
     end
+
+    test "Simple Multiple Queue Rate Limiting" do
+      range_one = 100..105
+      range_two = 200..210
+      monitor_ref = spawn(fn -> monitor_processing(%{"TestHandlerQueueRateOne" => Enum.count(range_one),
+                                                     "TestHandlerQueueRateTwo" => Enum.count(range_two)}) end) |>
+                    Process.monitor()
+
+      spawn(fn ->
+        for i <- range_one do
+          assert :ok == MessageHandler.receive_message("TestHandlerQueueRateOne", "TestMessage_#{i}")
+        end
+      end)
+
+      spawn(fn ->
+        for i <- range_two do
+          assert :ok == MessageHandler.receive_message("TestHandlerQueueRateTwo", "TestMessage_#{i}")
+        end
+      end)
+
+      assert_receive({:DOWN, ^monitor_ref, _, _, _}, 12000)
+    end
+
   end
 end
